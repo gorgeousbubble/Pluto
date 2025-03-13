9c56abd91ee191e38eff19c7861c73431675a197
Alopex6414 <1029535012@qq.com>
2025-03-13 22:15:02 +0800

    Add img2vector & handwritingClassTest functions

--- classifier/knn.py
+++ classifier/knn.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import array
+import os
 
 import numpy
 import operator
@@ -103,6 +104,40 @@ def classifyPerson():
     inArr = numpy.array([ffMiles, percentTats, iceCream])
     classifierResult = classify0((inArr-minVals)/ranges, normMat, datingLabels, 3)
     print("You will probably like this person:", resultList[classifierResult-1])
+
+def img2vector(filename):
+    returnVect = zeros((1, 1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0, i*32+j] = int(lineStr[j])
+    return returnVect
+
+def handwritingClassTest():
+    hwLabels = []
+    trainingFileList = os.listdir('trainingDigits')
+    m = len(trainingFileList)
+    trainingMat = numpy.zeros((m, 1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i, :] = img2vector('trainingDigits/%s' % fileNameStr)
+    testFileList = os.listdir('testDigits')
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
+        classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+        print("the classifier came back with: %d, the real answer is: %d" % (classifierResult, classNumStr))
+        if classifierResult != testFileList[i]: errorCount += 1.0
+    print("the total number of errors is: %d" % errorCount
+    print("the total error rate is: %f" % (errorCount / float(mTest)))
 
 if __name__ == '__main__':
     classifyPerson()
