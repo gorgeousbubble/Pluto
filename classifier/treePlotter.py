9e22e0699625274f313cda1586950ccb324871dd
Alopex6414 <1029535012@qq.com>
2025-03-24 22:47:11 +0800

    Add function

--- classifier/treePlotter.py
+++ classifier/treePlotter.py
@@ -6,12 +6,16 @@ decisionNode = dict(boxstyle="sawtooth", fc="0.8")
 leafNode = dict(boxstyle="round4", fc="0.8")
 arrow_args = dict(arrowstyle="<-")
 
-def createPlot():
+def createPlot(inTree):
     fig = matplotlib.pyplot.figure(1, facecolor='white')
     fig.clf()
-    createPlot.ax1 = matplotlib.pyplot.subplot(111, frameon=False)
-    plotNode('decision node', (0.5, 0.1), (0.1, 0.5), decisionNode)
-    plotNode('leaf node', (0.8, 0.1), (0.3, 0.8), leafNode)
+    axprops = dict(xticks=[], yticks=[])
+    createPlot.ax1 = matplotlib.pyplot.subplot(111, frameon=False, **axprops)
+    plotTree.totalW = float(getNumLeafs(inTree))
+    plotTree.totalD = float(getTreeDepth(inTree))
+    plotTree.xOff = -0.5/plotTree.totalW
+    plotTree.yOff = -0.1
+    plotTree(inTree, (0.5, 1.0), '')
     matplotlib.pyplot.show()
 
 def plotNode(nodeTxt, centerPt, parentPt, nodeType):
@@ -45,10 +49,33 @@ def retrieveTree(i):
     listOfTrees = [{'no surfacing': {0:'no', 1: {'flippers':{0: 'no', 1:'yes'}}}}, {'no surfacing':{0:'no', 1:{'flippers':{0: {'head':{0:'no', 1:'yes'}}, 1:'no'}}}}]
     return listOfTrees[i]
 
+def plotMidText(cntrPt, parentPt, txtString):
+    xMid = (parentPt[0] - cntrPt[0]) / 2.0 + cntrPt[0]
+    yMid = (parentPt[1] - cntrPt[1]) / 2.0 + cntrPt[1]
+    createPlot.ax1.text(xMid, yMid, txtString)
+
+def plotTree(myTree, parentPt, nodeTxt):
+    numLeafs = getNumLeafs(myTree)
+    depth = getTreeDepth(myTree)
+    firstStr = list(myTree.keys())[0]
+    cntrPt = (plotTree.xOff + (1.0 + float(numLeafs)) / 2.0 / plotTree.totalW, plotTree.yOff)
+    plotMidText(cntrPt, parentPt, nodeTxt)
+    plotNode(firstStr, cntrPt, parentPt, decisionNode)
+    secondDict = myTree[firstStr]
+    plotTree.yOff = plotTree.yOff - 1.0/plotTree.totalD
+    for key in secondDict.keys():
+        if type(secondDict[key]) == dict:
+            plotTree(secondDict[key], cntrPt, key)
+        else:
+            plotTree.xOff = plotTree.xOff + 1.0/plotTree.totalW
+            plotNode(secondDict[key], (plotTree.xOff, plotTree.yOff), cntrPt, leafNode)
+            plotMidText((plotTree.xOff, plotTree.yOff), cntrPt, key)
+    plotTree.yOff = plotTree.yOff + 1.0/plotTree.totalD
+
 if __name__ == '__main__':
     print("Retrieve Tree 1:", retrieveTree(1))
     myTree = retrieveTree(0)
     print("Leafs number of myTree:", getNumLeafs(myTree))
     print("Depth of myTree:", getTreeDepth(myTree))
-    createPlot()
+    createPlot(myTree)
     pass
