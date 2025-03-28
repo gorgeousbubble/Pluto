d6feb4225b06f1e87049132c6f584610a76ce818
Alopex6414 <1029535012@qq.com>
2025-03-28 22:15:28 +0800

    Add bayes.py

---
+++ classifier/bayes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def loadDataSet():
+    postingList = [
+        [
+            'my',
+            'dog',
+            'has',
+            'flea',
+            'problems',
+            'help',
+            'please'
+        ]
+    ]
+    classVec = [0, 1, 0, 1, 0, 1]
+    return postingList, classVec
+
+
+if __name__ == '__main__':
+    pass
